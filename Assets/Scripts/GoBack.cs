dc1cadf991b4dffd84cc9455b3a3035d745aa344
Magda Michalik <em.michalik@wp.pl>
2015-01-06 17:58:46 +0100

    [add] add missing colors

---
+++ Assets/Scripts/GoBack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("go back");
+		Destroy(GameObject.Find("Root"));
+		//Application.LoadLevel (Application.loadedLevel("Cube"));
+	}
+}
